9921b385a9258accfdf54e8da7e3e8c6e251013d
RAKANIDS <46525077+RAKANIDS@users.noreply.github.com>
2021-05-25 18:09:21 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,44 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+ 
+const devs = ["493135376442196010"];
+const adminprefix = ["="];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+ 
+  if (message.content.startsWith(adminprefix + 'pl')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "lev")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'li')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅ تم تغيير الحالة بنجاح **`)
+  }
+  if (message.content.startsWith(adminprefix + 'name')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**✅ تم تغير الاسم بنجاح ${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'avatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**✅ تم تغير الصور بنجاح ${argresult}** `);
+}
+});//hamo
+ 
+// THIS  MUST  BE  THIS  WAY
+client.login('NDkzMTM1Mzc2NDQyMTk2MDEw.Xp9PGQ.uPsjQjHiNnjiz0amFuaorcDRg-4');
